0c9b37fa63862afb83cf32d75f165423bc70f037
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2021-02-07 00:09:32 +0100

    Jumpscare

---
+++ New Unity Project/Assets/scripts/JumpTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class JumpTrigger : MonoBehaviour
+{
+    public AudioSource Scream;
+    public GameObject ThePlayer;
+    public GameObject JumpCam;
+    public GameObject FlashImg;
+
+    private void OnTriggerEnter()
+    {
+        Scream.Play();
+        JumpCam.SetActive(true);
+        ThePlayer.SetActive(false);
+        FlashImg.SetActive(true);
+        StartCoroutine(EndJump());
+    }
+
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(2.03f);
+        ThePlayer.SetActive(true);
+        
+        JumpCam.SetActive(false);
+        FlashImg.SetActive(false);
+    }
+}
