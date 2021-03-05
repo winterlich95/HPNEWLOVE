489a68f05f0041a390cf7c79ec1348522474c7de
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2021-03-05 13:05:31 +0100

    Terrain Reset

--- New Unity Project/Assets/scripts/MouseLook.cs
+++ New Unity Project/Assets/scripts/MouseLook.cs
@@ -20,7 +20,7 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation += mouseY;
+        xRotation -= mouseY;
 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
