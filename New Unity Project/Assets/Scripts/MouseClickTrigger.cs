cdee8f978e6c61783d33ee3238d4e3519e5c80e4
Abacadae <mirko.wallach13@gmail.com>
2021-03-06 18:03:28 +0100

    Maustrigger Anpassung
    Das Skript zum triggern von Funktionen klappt jetzt per Mausklick.
    Benutzt dafür MouseClickTrigger. Damit es funktioniert, braucht das Objekt, das angeklickt werden soll eins der vorgegebenen Tags.

---
+++ New Unity Project/Assets/Scripts/MouseClickTrigger.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClickTrigger : MonoBehaviour
+{
+    [SerializeField] public List<string> selectableTags;
+
+
+
+    public int distanceOfRay = 5000;
+    private RaycastHit _hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit _hit;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+
+            if (Physics.Raycast(ray, out _hit, 100.0f))
+            {
+                
+                if ((_hit.transform != null) && (selectableTags.Contains(_hit.transform.gameObject.tag)))
+                {
+                    CallFunction(_hit.transform);
+                }
+            }
+        }
+    }
+
+    public void CallFunction(Transform hitObject)
+    {
+            string tag = hitObject.gameObject.tag;
+            switch (tag)
+            {
+                case "JumpscareTrigger":
+                    hitObject.gameObject.GetComponent<JumpTrigger>().StartJump();
+                    break;
+
+                case "TextTrigger":
+                    hitObject.gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+                    break;
+
+                case "AudioTrigger2":
+                    hitObject.gameObject.GetComponent<AudioManager>().Play("Brief");
+                    break;
+
+                case "SceneTrigger":
+                    hitObject.gameObject.GetComponent<SceneChange>().ChangeScene();
+                    break;
+
+                case "Teleport":
+                    hitObject.gameObject.GetComponent<tp>().TeleportPlayer();
+                    break;
+
+                default:
+                    print("Das Objekt " + hitObject.gameObject.name + " hat keine Funktion.");
+                    break;
+            }
+        
+    }
+
+
+    public void CallJump()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+            _hit.transform.gameObject.GetComponent<JumpTrigger>().StartJump();
+
+        }
+
+
+    }
+
+    public void CallTeleport()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+            _hit.transform.gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+
+        }
+    }
+    public void CallText()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+            _hit.transform.gameObject.GetComponent<tp>().TeleportPlayer();
+
+        }
+
+
+    }
+
+
+    public void CallSound()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+            _hit.transform.gameObject.GetComponent<AudioManager>().Play("Brief");
+
+        }
+
+    }
+
+    public void CallImage()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+            _hit.transform.gameObject.GetComponent<CallLetter>().TeleportImage();
+
+        }
+    }
+
+    public void EndText()
+    {
+        FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+
+}
