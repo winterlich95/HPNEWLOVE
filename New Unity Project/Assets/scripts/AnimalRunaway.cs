0bf7a3a92d5cd9d3495db7c24cada6f7ab8765af
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2021-03-09 12:52:55 +0100

    Baum läuft zu gewünschten Punkt

---
+++ New Unity Project/Assets/scripts/AnimalRunaway.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class AnimalRunaway : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    Transform target;
+    Transform chasepoint;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        chasepoint = PlayerManager.instance.chasepoint.transform;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(chasepoint.position);
+            if (distance >= lookRadius)
+            {
+                //Attack
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+    }
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
+
+
