c18236c16e92a64bad5b3477f71f745db547b819
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2021-03-09 14:58:36 +0100

    update

--- New Unity Project/Assets/scripts/PlayerMovement.cs
+++ New Unity Project/Assets/scripts/PlayerMovement.cs
@@ -32,17 +32,19 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = -2f;
         }
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move* speed * Time.deltaTime );
-        if(Input.GetButtonDown("Jump") && isGrounded )
+         if(Input.GetButtonDown("Jump") && isGrounded )
         {
             velocity.y = Mathf.Sqrt(jumpHeight* -2f* gravity);
         }
         velocity.y += gravity*Time.deltaTime;
+        
         controller.Move(velocity * Time.deltaTime);
 
         if(Input.GetButtonDown("Cancel"))
