6aece7c0896c1eff4ae47606cd4c593166376b4d
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2021-04-09 19:32:03 +0200

    Gameoverscreen

---
+++ New Unity Project/Assets/GameOverScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+  
+    
+        public void OnCollisionEnter(Collision hitObject)
+        {
+            if (hitObject.collider.tag == "PlayerCollide")
+            {
+                Debug.Log("Collision");
+                gameObject.SetActive(true);
+                FindObjectOfType<GameManager>().EndGame();
+            }
+        }
+
+        public void ExiTButton()
+        {
+         SceneManager.LoadScene("");
+         }
+        
+        public void RestartButton()
+        {
+        SceneManager.LoadScene("");
+        }
+
+}
+
